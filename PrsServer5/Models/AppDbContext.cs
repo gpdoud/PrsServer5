092b0f9711d2a418fd978cb4367a6008329e1caf
gpdoud <gpdoud@gmail.com>
2021-03-04 22:49:24 -0500

    * .gitignore: Added partial User class
    * PrsServer5.sln:
    * Program.cs:
    * Startup.cs:
    * User.cs:
    * appsettings.json:
    * PrsServer5.csproj:
    * WeatherForecast.cs:
    * AppDbContext.cs:
    * appsettings.Development.json:
    * UsersController.cs:
    * launchSettings.json:
    * 20210305031221_Init.cs:
    * AppDbContextModelSnapshot.cs:
    * WeatherForecastController.cs:
    * 20210305031221_Init.Designer.cs:

---
+++ PrsServer5/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace PrsServer5.Models {
+
+    public class AppDbContext : DbContext {
+
+        public virtual DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+
+        }
+
+    }
+}
---
+++ PrsServer5/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PrsServer5.Models {
+    public class User {
+
+        public int  Id { get; set; }
+        public string Username { get; set; }
+
+        public User() {
+        }
+    }
+}
