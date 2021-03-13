1f43e80ff942c8877b4e1e8b14de1e1764d1bb1e
Greg Doud <gpdoud@gmail.com>
2021-03-13 08:09:31 -0500

    Fixed User.Login

--- PrsServer5/Controllers/UsersController.cs
+++ PrsServer5/Controllers/UsersController.cs
@@ -17,12 +17,12 @@ namespace PrsServer5.Controllers {
             _context = context;
         }
 
-        // GET: api/Users/login/{username}/{password}
-        [HttpGet("login/{username}/{password}")]
+        // GET: api/Users/{username}/{password}
+        [HttpGet("{username}/{password}")]
         public async Task<ActionResult<User>> Login(string username, string password) {
             var user = await _context.Users
-                .SingleOrDefaultAsync(u => u.Username.Equals(username, StringComparison.OrdinalIgnoreCase)
-                                        && u.Password.Equals(password, StringComparison.Ordinal));
+                .SingleOrDefaultAsync(u => u.Username.ToUpper().Equals(username.ToUpper())
+                                        && u.Password.Equals(password));
             return (user == null)
                 ? NotFound()
                 : user;
