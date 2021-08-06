60ba773978ae976c07cb138c2727990f5c0f318c
Greg Doud <gpdoud@gmail.com>
2021-08-06 13:58:32 -0400

    "Final commit"

---
+++ PrsServer5/ViewModel/UserView.cs
@@ -0,0 +1,13 @@
+﻿using PrsServer5.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrsServer5.ViewModel {
+    public class UserView {
+
+        public UserView(User u) { }
+    }
+}
